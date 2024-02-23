ed1b873d99ca67b44afff5a2e5acfe50539b44fb
kimhaseon <156289947+kimhaseon@users.noreply.github.com>
2024-02-23 23:58:47 +0900

    react-app

---
+++ src/coin.js
@@ -0,0 +1,40 @@
+/*import { useEffect, useState } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [myMoney, setMyMoney] = useState([]);
+   
+  useEffect(() => {
+   fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+     setCoins(json);
+     setLoading(false);
+  });
+  }, []);
+
+  function onChange(event) {
+    setMyMoney(event.target.value)
+  }
+
+  return ( 
+  <div> 
+  <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
+   <input
+    onChange={onChange}
+    value={myMoney}
+    type="number"
+    placeholder="Please write your USD"
+    />
+
+   {loading ? <strong>Loading...</strong> : <select>
+    {coins.map((coin) => <option> {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD </option>)}
+    {coins.map((coin) => <option>{coin.name} ({coin.symbol}): {myMoney / coin.quotes.USD.price} {coin.symbol}</option>)}
+   </select> 
+   }
+  </div>
+  );
+  }
+
+export default App;*/
---
+++ src/test.js
@@ -0,0 +1,55 @@
+/*import Button from "./Button";
+import styles from "./Button.module.css";
+
+function App() {
+    return(
+      <div>
+        <h1 className={styles.title}>Welcome back!</h1>
+      <Button text={"Continue"}/>
+      </div>
+    );
+  }*/
+
+
+  /*import { useState } from "react";
+
+
+
+  /*import { useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo,...currentArray]);
+    setToDo("");
+  };
+  
+  return ( 
+  <div>
+    <h1>My To Dos({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+    <input 
+    onChange={onChange} 
+    value={toDo} 
+    type="text" 
+    placeholder="Write your to do"
+    />
+    <button>Add To</button>
+    </form>
+    <hr />
+    <ul>
+    {toDos.map((item, index) => (
+    <li key={index}>{item}</li> 
+    ))}
+    </ul>
+  </div>
+  );
+}
+
+export default App;*/
